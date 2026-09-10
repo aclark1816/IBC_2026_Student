d25530e0af5885b9b8110cd41275139f0d834460
aclark1816 <personal.alexei@gmail.com>
2026-09-10 10:59:00 -0400

    Refactor comments in Clark_Semicolon_Converter.sh
    Updated comments for clarity and consistency.

--- Practicals/Pract_Week3/Clark_Semicolon_Converter.sh
+++ Practicals/Pract_Week3/Clark_Semicolon_Converter.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
 # $1 takes the file passed as an argument
 input_file="$1"
 
-# Creates a new filename by appending '_converted.csv'
+# Creates output file
 output_file="${input_file%.*}_converted.csv"
 
-# Replace semicolons with commas and write out to the new file
+# Replace semicolons with commas, write out new files
 tr ';' ',' < "$input_file" > "$output_file"
 
 echo "Converted $input_file and saved as $output_file"
