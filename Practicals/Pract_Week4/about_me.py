863a9db495e8bb91ccd87f5116f054ee56289a8e
aclark1816 <personal.alexei@gmail.com>
2026-09-16 18:24:47 -0400

    Refactor about_me.py for clarity and conciseness
    Removed unnecessary comments and cleaned up code formatting.

--- Practicals/Pract_Week4/about_me.py
+++ Practicals/Pract_Week4/about_me.py
@@ -1,41 +1,15 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
 
 # The following code aims to print an "all about you" panel, with each characteristic saved as a variable
-
-
-# In[4]:
-
-
 # Saving each characteristic as a variable
-
-
-# In[8]:
-
 
 name = "Alexei Clark" # Name
 fav_color = "Blue" # Favorite Color
 fav_activ = "Playing Video Gamees" # Favorite Activity
 fav_anim = "Kiwi" # Favorite Animal
 
-
-# In[17]:
-
-
 # Print out full panel, separating text and variables
-
-
-# In[18]:
-
-
-# Use \n to separate lines
-
-
-# In[15]:
-
+# Used \n to separate lines
 
 print("My Name: " + name +
      "\nMy Favorite Color: " + fav_color +
