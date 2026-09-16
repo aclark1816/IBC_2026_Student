c3e0f9a1afa44432592608f69b30719dd5d553b6
aclark1816 <personal.alexei@gmail.com>
2026-09-16 18:23:27 -0400

    Add files via upload

---
+++ Practicals/Pract_Week4/about_me.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# The following code aims to print an "all about you" panel, with each characteristic saved as a variable
+
+
+# In[4]:
+
+
+# Saving each characteristic as a variable
+
+
+# In[8]:
+
+
+name = "Alexei Clark" # Name
+fav_color = "Blue" # Favorite Color
+fav_activ = "Playing Video Gamees" # Favorite Activity
+fav_anim = "Kiwi" # Favorite Animal
+
+
+# In[17]:
+
+
+# Print out full panel, separating text and variables
+
+
+# In[18]:
+
+
+# Use \n to separate lines
+
+
+# In[15]:
+
+
+print("My Name: " + name +
+     "\nMy Favorite Color: " + fav_color +
+     "\nMy Favorite Activity: " + fav_activ +
+     "\nMy Favorite Animal: " + fav_anim)
+
